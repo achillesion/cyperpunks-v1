16789ae3a3ec8c549452ad71c070a000a1dac7e8
achillesion <achillesion@gmail.com>
2022-01-13 17:46:28 +0500

    CI=false

--- src/App.js
+++ src/App.js
@@ -15,10 +15,6 @@ function App() {
   // API call via useEffect Async Await
   useEffect(() => {
   const getMyNfts = async () => {
-    const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0xb6F41E1cb14df0F8e84e86090eAF6802354E8d02&order_direction=asc'
-    )
-     console.log(openseaData.data.assets)
-
     setPunkListData(stockData);
 
   }
